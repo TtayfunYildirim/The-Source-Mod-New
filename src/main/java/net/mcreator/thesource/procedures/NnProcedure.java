438c8a97a43e212fa13f92dd1262237bd51d624f
TtayfunYildirim <93596249+Tayfun157@users.noreply.github.com>
2023-02-18 00:42:12 +0300

    deneme

---
+++ src/main/java/net/mcreator/thesource/procedures/NnProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.thesource.procedures;
+
+import java.util.Map;
+
+public class NnProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
