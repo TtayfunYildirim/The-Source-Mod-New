a390abb89dd84cbe45fba10b17edfb91a1c855f1
TtayfunYildirim <93596249+Tayfun157@users.noreply.github.com>
2023-02-18 00:37:55 +0300

    d

---
+++ src/main/java/net/mcreator/thesource/procedures/AaProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.thesource.procedures;
+
+import java.util.Map;
+
+public class AaProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
